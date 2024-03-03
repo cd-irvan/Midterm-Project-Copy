ab0b16d2c1b19616b31e3d892e4df245065bbf42
ayyubshaffy7 <44515527+ayyubshaffy7@users.noreply.github.com>
2024-03-03 22:20:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,9 @@ Insurance_data_encoded = pd.get_dummies(Insurance_data, columns=['sex', 'smoker'
 
 st.sidebar.header("Dashboard")
 st.sidebar.markdown("---")
-app_mode = st.sidebar.selectbox('Select Page',['Introduction','Visualization','Prediction'])
+#app_mode = st.sidebar.selectbox('Select Page',['Introduction','Visualization','Prediction'])\
+pages = ['Introduction', 'Visualization', 'Prediction']
+app_mode = st.sidebar.radio("Select Page", pages)
 list_variables = Insurance_data.columns
 
 
