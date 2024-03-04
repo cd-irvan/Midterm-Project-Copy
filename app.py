23e7bc074968e48421605d379f95f4440fdd1092
rohan-26 <88795185+rohan-26@users.noreply.github.com>
2024-03-04 16:50:56 +0400

    Update app.py

--- app.py
+++ app.py
@@ -319,16 +319,6 @@ if app_mode == "Prediction":
     equation = f"y = {np.round(intercept, 2)}"
     for feature, coeff in zip(feature_names, coefficients):
         equation += f" + ({np.round(coeff, 2)}) \\times {feature}"
-        
-    # Display the equation
-    st.markdown("The equation of the linear regression model for predicting insurance premium is:")
-
-    # Split the equation into multiple lines for better display
-    equation_parts = [equation[i:i+50] for i in range(0, len(equation), 50)]
-    equation_display = " ".join(equation_parts)
-    
-    # Display the equation using LaTeX
-    st.latex(r"{}".format(equation_display))
 
     st.markdown("### Equation of the Linear Regression Model for Predicting Insurance Premiums")
 
